2998b51d59101b70576bfa9a2768ede91f21fb98
Muhiddin Uzan <muhiddinuzan@outlook.com>
2022-03-30 02:51:40 +0300

    Translate

--- app.js
+++ app.js
@@ -15,33 +15,33 @@ new Vue({
             var point = Math.ceil(Math.random() * 10);
             this.monster_health -= point;
             this.monster_attack();
-            this.add_to_log({turn : "Player", text : "Oyuncu Atagi (" + point + ")"});
+            this.add_to_log({turn : "Player", text : "Player Attack (" + point + ")"});
         },
 
         special_attack : function() {
             var point = Math.ceil(Math.random() * 25);
             this.monster_health -= point;
             this.monster_attack();
-            this.add_to_log({turn : "Player", text : "Ozel Oyuncu Atagi (" + point + ")"});
+            this.add_to_log({turn : "Player", text : "Special Player Attack (" + point + ")"});
         },
 
         heal_up : function() {
             var point = Math.ceil(Math.random() * 25);
             this.me_health += point;
             this.monster_attack();
-            this.add_to_log({turn : "Player", text : "Ilk Yardim (" + point + ")"});
+            this.add_to_log({turn : "Player", text : "Heal (" + point + ")"});
         },
 
         give_up : function() {
             this.me_health = 0; 
-            this.add_to_log({turn : "Player", text : "Oyuncu Pes Etti..."});
+            this.add_to_log({turn : "Player", text : "Player Give Up..."});
     
 
         },
         monster_attack : function() {
             var point = Math.ceil(Math.random() * 10);
             this.me_health -= point;
-            this.add_to_log({turn : "Monster", text : "Canavar Atagi (" + point + ")"});
+            this.add_to_log({turn : "Monster", text : "Monster Attack (" + point + ")"});
         },
         add_to_log : function(log) {
             this.logs.push(log);
@@ -51,7 +51,7 @@ new Vue({
         me_health : function(value) {
             if (value <= 0) {
                 this.me_health = 0;
-                if(confirm("Oyunu Kaybettin... Tekrar oynamak ister misin?")) {
+                if(confirm("You Lose! New Game?")) {
                     this.me_health = 100;
                     this.monster_health = 100;
                     this.logs = [];
@@ -63,7 +63,7 @@ new Vue({
         monster_health : function(value) {
             if (value <= 0) {
                 this.monster_health = 0;
-                if(confirm("Oyunuzu Kazandın, Tebrikler! Tekrar Denemeye Var Misin?")) {
+                if(confirm("You Win! New Game?")) {
                     this.me_health = 100;
                     this.monster_health = 100;
                     this.logs = [];
